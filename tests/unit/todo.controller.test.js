1db84d67c20a11d219d560af11cc11d5daa69442
priest <pzoli828@gmail.com>
2019-09-14 16:39:42 +0200

    test with jest mock model

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,7 +1,15 @@
 const TodoController = require('../../controllers/todo.controller');
+const TodoModel = require('../../model/todo.model');
+
+// mock function
+TodoModel.create = jest.fn();
 
 describe("TodoController.createTodo", () => {
-  it("schould have a createTodo function", () => {
+  it("should have a createTodo function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
+  });
+  it("should call TodoModel.create", () => {
+    TodoController.createTodo();
+    expect(TodoModel.create).toBeCalled();
   });
 });
